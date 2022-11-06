6861a72e18d7347cd0f7ca624848805560c36fd1
Daniel Freitas <danielluiz358@gmail.com>
2022-11-06 19:41:20 -0300

    formulário login

---
+++ script.js
@@ -0,0 +1,7 @@
+const loginForm = document.querySelector('.form-login')
+const loginBtn = document.querySelector('#login-btn')
+const registerBtn = document.querySelector("#register")
+const inputUsername = document.querySelector('#username')
+const inputPassword = document.querySelector('#password')
+const footer = document.querySelector(".footer")
+
