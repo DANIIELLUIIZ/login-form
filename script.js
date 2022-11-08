2ad9c8be5374866b98f317324a4d2b9e2ba286ef
Daniel Freitas <danielluiz358@gmail.com>
2022-11-07 21:17:18 -0300

    Criação do formulário de cadastro

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const loginForm = document.querySelector('.form-login')
-const loginBtn = document.querySelector('#login-btn')
-const registerBtn = document.querySelector("#register")
-const inputUsername = document.querySelector('#username')
-const inputPassword = document.querySelector('#password')
-const footer = document.querySelector(".footer")
+const loginTitle = document.querySelector('.login-title')
+const footer = document.querySelector('.footer')
+const registerForm = document.querySelector('.register-form')
+const registerBtn = document.querySelector('#register')
+const sendRegisterBtn = document.querySelector('#send-register-btn')
+const cancelRegisterBtn = document.querySelector('#cancel-register-btn')
 
+// ======================== FUNÇÕES =======================//
+
+const toggleForms = () => {
+  loginForm.classList.toggle('hide')
+  footer.classList.toggle('hide')
+  registerForm.classList.toggle('hide')
+  loginTitle.classList.toggle('hide')
+}
+
+// ======================== Eventos =======================//
+
+registerBtn.addEventListener('click', () => {
+  toggleForms()
+})
+
+cancelRegisterBtn.addEventListener('click', () => {
+  e.preventDefault()
+  toggleForms()
+})
